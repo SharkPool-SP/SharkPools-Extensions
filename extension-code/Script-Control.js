344a2b93898f0216824180cebea10ebab07154f7
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-02-25 23:18:38 -0800

    Update Script-Control.js

--- extension-code/Script-Control.js
+++ extension-code/Script-Control.js
@@ -323,7 +323,6 @@ function addLinearGradientToBody() {
 if (typeof scaffolding === "undefined") addLinearGradientToBody();
 
 function documentChangedCallback(mutationsList, observer) {
-  var elements = document.querySelectorAll("g[data-category=\"Script Control\"] path");
   var pathElements = document.querySelectorAll("g[data-category=\"Script Control\"] path");
   pathElements.forEach(function(pathElement) {
     var currentFill = pathElement.getAttribute("fill");
