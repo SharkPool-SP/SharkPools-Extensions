ef299bf4cdb12982237f0e64d554ceee34829ebb
LittleBlueGamer <148423893+LittleBlueGamer427@users.noreply.github.com>
2024-03-07 19:54:03 -0800

    Update Pixel-Utilities.js

--- extension-code/Pixel-Utilities.js
+++ extension-code/Pixel-Utilities.js
@@ -1,101 +1,140 @@
 // Name: Pixel Utilities
 // ID: lbgpixelutilities
 // Description: Random Utility Blocks by LittleBlueGamer
-// By: LittleBlueGamer427
+// By: LittleBlueGamer
 
-// Version V.1.0.0
+// Version V1.1.0
 
-(function (Scratch) {
-  "use strict";
+(function (Scratch) {0
+    "use strict";
 
-  class GarbageUtilities {
-    getInfo() {
-      return {
-        id: "lbgpixelutilities",
-        name: "Pixel Utilities",
-        color1: "#0085ff",
-        color2: "#0069cb",
-        color3: "#004d94",
-        blocks: [
-          {
-            opcode: "percentChance",
-            blockType: Scratch.BlockType.BOOLEAN,
-            text: "[percent] % chance of returning true",
-            arguments: {
-              percent: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 50
-              }
-            },
-          },
-          {
-            opcode: "percentOfNumber",
-            blockType: Scratch.BlockType.REPORTER,
-            text: "[percent]% of [number]",
-            arguments: {
-              percent: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 50
-              },
-              number: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 100
-              }
-            },
-          },
-          {
-            opcode: "reverseString",
-            blockType: Scratch.BlockType.REPORTER,
-            text: "reverse [text]",
-            arguments: {
-              text: {
-                type: Scratch.ArgumentType.STRING,
-                defaultValue: "roltycore 2"
-              }
-            },
-          },
-          {
-            opcode: "getPercentageOfSteps",
-            blockType: Scratch.BlockType.REPORTER,
-            text: "from [stepOrigin], get [percentageInput]% of the steps to [stepLocation]",
-            arguments: {
-              stepOrigin: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 0
-              },
-              percentageInput: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 50
-              },
-              stepLocation: {
-                type: Scratch.ArgumentType.NUMBER,
-                defaultValue: 100
-              }
-            },
-          },
-        ],
-      };
+    class GarbageUtilities {
+        getInfo() {
+            return {
+                id: 'lbgpixelutilities',
+                name: 'Pixel Utilities',
+                color1: "#0085ff",
+                color2: "#0069cb",
+                color3: "#004d94",
+                blocks: [
+                    {
+                        blockType: Scratch.BlockType.HAT,
+                        opcode: 'whenConditionGoesYes',
+                        text: 'when [CONDITION1] goes Y E S',
+                        isEdgeActivated: true,
+                        arguments: {
+                            CONDITION1: {
+                                type: Scratch.BlockType.BOOLEAN
+                            },
+                        }
+                    },
+                    {
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        opcode: 'trueOrFalseBoolean',
+                        text: '[trueorfalse]',
+                        arguments: {
+                            trueorfalse: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: 'true',
+                                menu: 'trueorfalsemenu'
+                            }
+                        }
+                    },
+                    {
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        opcode: "isGamePackaged",
+                        text: 'is this game packaged',
+                    },
+                    {
+                        opcode: 'percentChance',
+                        blockType: Scratch.BlockType.BOOLEAN,
+                        text: '[percent] chance of returning true',
+                        arguments: {
+                            percent: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 50,
+                            },
+                        },
+                    },
+                    {
+                        opcode: 'percentOfNumber',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: '[percent]% of [number]',
+                        arguments: {
+                            percent: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 50,
+                            },
+                            number: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 100,
+                            },
+                        },
+                    },
+                    {
+                        opcode: 'reverseString',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: 'reverse [text]',
+                        arguments: {
+                            text: {
+                                type: Scratch.ArgumentType.STRING,
+                                defaultValue: 'roltycore 2',
+                            },
+                        },
+                    },
+                    {
+                        opcode: 'getPercentageOfSteps',
+                        blockType: Scratch.BlockType.REPORTER,
+                        text: 'from [stepOrigin], get [percentageInput]% of the steps to [stepLocation]',
+                        arguments: {
+                            stepOrigin: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 0,
+                            },
+                            percentageInput: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 50,
+                            },
+                            stepLocation: {
+                                type: Scratch.ArgumentType.NUMBER,
+                                defaultValue: 100,
+                            },
+                        },
+                    },
+                ],
+                menus: {
+                    trueorfalsemenu: {
+                        acceptReporters: true,
+                        items: ['true', 'false'],
+                    }
+                },
+            };
+        }
+        whenConditionGoesYes({CONDITION1}) {
+            return CONDITION1;
+        }
+        trueOrFalseBoolean(args) {
+            return args.trueorfalse
+        }
+        isGamePackaged() {
+            return Scratch.vm.runtime.isPackaged
+        }
+        percentChance(args) {
+            return Math.random() < args.percent / 100;
+        }
+        percentOfNumber(args) {
+            return (args.percent / 100) * args.number;
+        }
+        reverseString(args) {
+            return args.text.split('').reverse().join('');
+        }
+        getPercentageOfSteps(args) {
+            const origin = args.stepOrigin;
+            const percentage = args.percentageInput / 100;
+            const distance = args.stepLocation - origin;
+            return distance * percentage;
+        }
     }
 
-    percentChance(args) {
-      return Math.random() < args.percent / 100;
-    }
-
-    percentOfNumber(args) {
-      return (args.percent / 100) * args.number;
-    }
-
-    reverseString(args) {
-      return args.text.split("").reverse().join("");
-    }
-
-    getPercentageOfSteps(args) {
-      const origin = args.stepOrigin;
-      const percentage = args.percentageInput / 100;
-      const distance = args.stepLocation - origin;
-      return distance * percentage;
-    }
-  }
-
-  Scratch.extensions.register(new GarbageUtilities());
+    Scratch.extensions.register(new GarbageUtilities());
 })(Scratch);
