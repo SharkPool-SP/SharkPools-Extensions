ea938e3a10978c0a5f2f29eaa0ba84b6e94f3a9c
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-06-07 21:36:49 -0700

    Update Tune-Shark-V3.js

--- extension-code/Tune-Shark-V3.js
+++ extension-code/Tune-Shark-V3.js
@@ -30,7 +30,7 @@
   const startFlag =
 "data:image/svg+xml;base64,PHN2ZyBpZD0iTGF5ZXJfMSIgZGF0YS1uYW1lPSJMYXllciAxIiB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAxNi42MyAxNy41Ij48ZGVmcz48c3R5bGU+LmNscy0xLC5jbHMtMntmaWxsOiM0Y2JmNTY7c3Ryb2tlOiM0NTk5M2Q7c3Ryb2tlLWxpbmVjYXA6cm91bmQ7c3Ryb2tlLWxpbmVqb2luOnJvdW5kO30uY2xzLTJ7c3Ryb2tlLXdpZHRoOjEuNXB4O308L3N0eWxlPjwvZGVmcz48dGl0bGU+aWNvbi0tZ3JlZW4tZmxhZzwvdGl0bGU+PHBhdGggY2xhc3M9ImNscy0xIiBkPSJNLjc1LDJBNi40NCw2LjQ0LDAsMCwxLDguNDQsMmgwYTYuNDQsNi40NCwwLDAsMCw3LjY5LDBWMTIuNGE2LjQ0LDYuNDQsMCwwLDEtNy42OSwwaDBhNi40NCw2LjQ0LDAsMCwwLTcuNjksMCIvPjxsaW5lIGNsYXNzPSJjbHMtMiIgeDE9IjAuNzUiIHkxPSIxNi43NSIgeDI9IjAuNzUiIHkyPSIwLjc1Ii8+PC9zdmc+";
 
-  // Pizzicato Library (Web Audio API, but with Premade Effects and Stuff) -- Modifed File
+  // Pizzicato Library (Web Audio API, but with Premade Effects and Stuff) -- Modified File
   // uses MIT License
   const scriptElement = document.createElement("script");
   scriptElement.textContent = 
@@ -939,15 +939,6 @@ a.Util.isInRange(e,0,1)&&(this.options.mix=e,this.dryGainNode.gain.value=s.Util.
         });
         this.updateEffect(lowpass, sound, "LOWPASS", args);
       }
-    }
-
-    setBit(args) {
-      const sound = soundBank[args.NAME];
-      if (sound === undefined) return;
-      const bitcrush = new Pizzicato.Effects.Bitcrusher({
-        bitDepth: Scratch.Cast.toNumber(args.FREQ), frequencyReduction: Scratch.Cast.toNumber(args.PEAK)
-      });
-      this.updateEffect(bitcrush, sound, "BITCRUSHER", args);
     }
 
     setFlanger(args) {
