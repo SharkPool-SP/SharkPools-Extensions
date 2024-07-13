52ca9a643cd915bf3708d6cf92daf92f84846501
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-07-12 21:25:03 -0700

    Color-Master -- Newer Gradient Patch

--- extension-code/Color-Master.js
+++ extension-code/Color-Master.js
@@ -3,7 +3,7 @@
 // Description: Color Utilities and Color Conversions
 // By: SharkPool
 
-//  Version 1.2.11
+//  Version 1.2.12
 
 (function (Scratch) {
   "use strict";
@@ -326,13 +326,13 @@
   }
   if (Scratch.gui) Scratch.gui.getBlockly().then((ScratchBlocks) => {
     addLinearGradientToBody();
-    if (!ScratchBlocks?.SPgradients?.patched) { // New Gradient Patch by 0znzw <3
+    if (!ScratchBlocks?.SPgradients?.patched) { // Gradient Patch by 0znzw & SharkPool
       ScratchBlocks.SPgradients = {gradientUrls: {}, patched: false};
       const BSP = ScratchBlocks.BlockSvg.prototype, BSPR = BSP.render;
       BSP.render = function(...args) {
         const res = BSPR.apply(this, args);
         let category;
-        if (this?.svgPath_ && (category = this.type.slice(0, this.type.indexOf("_"))) && ScratchBlocks.SPgradients.gradientUrls[category]) {
+        if (this?.svgPath_ && this?.category_ && (category = this.type.slice(0, this.type.indexOf("_"))) && ScratchBlocks.SPgradients.gradientUrls[category]) {
           const urls = ScratchBlocks.SPgradients.gradientUrls[category];
           if (urls) this.svgPath_.setAttribute("fill", urls[0]);
         }
