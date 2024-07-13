4729071c27c7c3ba6095d4458ad654b1f0f1526c
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-07-12 21:26:14 -0700

    Update Runtime-Events.js

--- extension-code/Runtime-Events.js
+++ extension-code/Runtime-Events.js
@@ -3,7 +3,7 @@
 // Description: Events from the Runtime
 // By: SharkPool
 
-// Version V.1.1.2
+// Version V.1.1.21
 
 (function (Scratch) {
   "use strict";
@@ -188,13 +188,13 @@
   }
   if (Scratch.gui) Scratch.gui.getBlockly().then((ScratchBlocks) => {
     addLinearGradientToBody();
-    if (!ScratchBlocks?.SPgradients?.patched) { // New Gradient Patch by 0znzw <3
+    if (!ScratchBlocks?.SPgradients?.patched) { // Gradient Patch by 0znzw & SharkPool
       ScratchBlocks.SPgradients = {gradientUrls: {}, patched: false};
       const BSP = ScratchBlocks.BlockSvg.prototype, BSPR = BSP.render;
       BSP.render = function(...args) {
         const res = BSPR.apply(this, args);
         let category;
-        if (this?.svgPath_ && (category = this.type.slice(0, this.type.indexOf("_"))) && ScratchBlocks.SPgradients.gradientUrls[category]) {
+        if (this?.svgPath_ && this?.category_ && (category = this.type.slice(0, this.type.indexOf("_"))) && ScratchBlocks.SPgradients.gradientUrls[category]) {
           const urls = ScratchBlocks.SPgradients.gradientUrls[category];
           if (urls) this.svgPath_.setAttribute("fill", urls[0]);
         }
