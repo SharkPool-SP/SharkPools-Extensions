aafb211c95ba59c2c4c7a7e658c5c23f767f3c7f
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2024-11-28 20:23:23 -0800

    Update extension-generator.js

--- pages/scripts/extension-generator.js
+++ pages/scripts/extension-generator.js
@@ -1,11 +1,13 @@
-function displayExts (json) {
+function displayExts(json) {
+  const oldDiv = document.querySelector(".ext-div");
+  if (oldDiv) oldDiv.remove();
+
   let tags = [];
   const main = document.createElement("div");
   main.classList.add("ext-div");
   main.setAttribute("style", "pointer-events: auto; width: 100%;");
 
   Object.entries(json).forEach((item) => {
-    if (item[0] === "Example") return;
     const info = item[1];
 
     const holderDiv = document.createElement("div");
@@ -14,6 +16,7 @@ function displayExts (json) {
 
     const img = document.createElement("img");
     img.id = item[0];
+    img.setAttribute("loading", "lazy");
     img.src = `/extension-thumbs/${item[0]}.svg`;
 
     const tag = info.status ? genTag(info.status) : "";
@@ -26,7 +29,7 @@ function displayExts (json) {
     };
 
     holderDiv.addEventListener("click", () => {
-      console.log(info)
+      console.log(info);
     });
     holderDiv.addEventListener("mouseenter", () => {
       holderDiv.animate([{ transform: "scale(1)" }, { transform: "scale(1.1)" }], { duration: 200, easing: "ease-in-out" });
