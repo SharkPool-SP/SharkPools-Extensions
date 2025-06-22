3795da694961e9963b5720662c6321a3980fcd63
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-06-21 21:12:17 -0700

    mobile-scaler.js -- fix flashing

--- pages/scripts/mobile-scaler.js
+++ pages/scripts/mobile-scaler.js
@@ -23,11 +23,10 @@ function rescaler() {
   const children = Array.from(extDiv.children);
   const breakerIndex = children.findIndex((e) => e.getAttribute("class") === "ext-breaker");
   const checkerIndex = breakerIndex === -1 ? 0 : breakerIndex + 2;
+  extDiv.setAttribute("style", "");
   if (
-    extDiv.children[checkerIndex].getBoundingClientRect().y === extDiv.children[checkerIndex + 1].getBoundingClientRect().y
+    extDiv.children[checkerIndex].getBoundingClientRect().y !== extDiv.children[checkerIndex + 1].getBoundingClientRect().y
   ) {
-    extDiv.setAttribute("style", "");
-  } else {
     extDiv.style.width = "250%";
     extDiv.style.left = "-75%";
     extDiv.style.transformOrigin = "top center";
