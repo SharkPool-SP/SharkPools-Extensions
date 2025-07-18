f57ece1ef7188b94d6adbd61a188df053227b6a2
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2025-07-17 22:17:20 -0700

    Update Community-Spotlight.js

--- extension-code/Community-Spotlight.js
+++ extension-code/Community-Spotlight.js
@@ -4,7 +4,7 @@
 // By: SharkPool
 // Licence: MIT
 
-// Version V.1.0.11
+// Version V.1.0.12
 
 (function (Scratch) {
   "use strict";
@@ -31,6 +31,8 @@
   // eslint-disable-next-line
   window.CommunitySpotlight={b:"https://raw.githubusercontent.com/Community-Spotlight/",u:{},c:{}};window.CommunitySpotlight.u["fetchIndex"]=async function(){const r=await fetch(`${window.CommunitySpotlight.b}promotion-index/main/index.json`);if(!r.ok)throw new Error("Couldn't fetch promotions!");return await r.json()};window.CommunitySpotlight.u["filterPromos"]=function(j,t,p){const r=(a)=>a[Math.floor(Math.random()*a.length)];t=t==="video"?"video":t==="html"?"html":"image";p=typeof p==="object"?p:{};if(p.tags){if(p.tags.constructor.name==="Array"){p.tags=p.tags.map((e)=>{return e.toLowerCase()})}else{console.warn("CS -- 'tags' parameter must be an Array");return{}}}const{aspectRatio,videoLength}=p;let c=Object.values(j).filter(q=>{return q.media[t+"s"].length>0});if(c.length===0){console.warn("CS -- No promotions found with given type");return {}}if(p.tags&&p.tags.length>0)c=c.filter((q)=>q.tags.some(tag=>p.tags.includes(tag)));if(aspectRatio)c=c.filter(q=>{return q.media[t+"s"].some((i)=>{return i.size===aspectRatio})});if(t==="video"&&videoLength)c=c.filter(q=>{return q.media.videos.some((i)=>{return i.length===videoLength})});if(c.length===0){console.warn("CS -- No promotions found with given parameters");return{}}const g=r(c);let m,l,f;switch(t){case"video":f=g.media.videos,m=aspectRatio&&videoLength?f.find(e=>e.size===aspectRatio&&e.length===videoLength):!aspectRatio&&videoLength?f.find(e=>e.length===videoLength):aspectRatio&&!videoLength?f.find(e=>e.size===aspectRatio):r(f),l=`sz${m.size.replace(":","x")}leng${m.length}.${m.type}`;break;case"html":f=g.media.htmls,l=`sz${(m=aspectRatio?f.find(e=>e.size===aspectRatio):r(f)).size.replace(":","x")}.html`;break;default:f=g.media.images,m=aspectRatio?f.find(e=>e.size===aspectRatio):r(f),l=`${m.size}.${m.type}`}delete g.media;g.url=`${window.CommunitySpotlight.b}promotion-media/main/${encodeURIComponent(g.id)}/${l}`;return g};async function refreshPromoCacheCS(){try{window.CommunitySpotlight.c = await window.CommunitySpotlight.u["fetchIndex"]()}catch (e){console.error(e)}}async function getOnlinePromoCS(t,p){return window.CommunitySpotlight.u["filterPromos"](await window.CommunitySpotlight.u["fetchIndex"](),t,p)}function getCachedPromoCS(t,p){return window.CommunitySpotlight.u["filterPromos"](structuredClone(window.CommunitySpotlight.c),t,p)}
 
+  const color1 = "#00a1e6";
+
   class SPspotlight {
     constructor() {
       // Initialize the Promotions
@@ -54,7 +56,7 @@
       return {
         id: "SPspotlight",
         name: "Community Spotlight",
-        color1: "#00a1e6",
+        color1,
         color2: "#0085e6",
         color3: "#006fbf",
         menuIconURI,
@@ -326,39 +328,37 @@
     }
   }
 
-  function add2Body() {
-    var grad = document.createElement("div");
-    grad.innerHTML = `<svg><defs>
-      <linearGradient x1="220" y1="-30" x2="240" y2="100" gradientUnits="userSpaceOnUse" id="SPspotlight-GRAD">
-      <stop offset="0" stop-color="#00d970"/><stop offset=".4" stop-color="#00aad9"/></linearGradient>
-      </defs></svg>`;
-    document.body.appendChild(grad);
-  }
-  if (Scratch.gui) Scratch.gui.getBlockly().then((SB) => {
+    if (Scratch.gui) Scratch.gui.getBlockly().then((SB) => {
+    function add2Body() {
+      const grad = document.querySelector(`div[class="SPgradCache"]`) || document.createElement("div");
+      grad.setAttribute("class", "SPgradCache");
+      grad.innerHTML = `
+        ${grad.innerHTML}
+        <svg><defs>
+          <linearGradient x1="220" y1="-30" x2="240" y2="100" gradientUnits="userSpaceOnUse" id="SPspotlight-GRAD">
+          <stop offset="0" stop-color="#00d970"/><stop offset=".4" stop-color="#00aad9"/></linearGradient>
+        </defs></svg>`;
+      document.body.append(grad);
+    }
     add2Body();
     if (!SB?.SPgradients?.patched) {
-      // Gradient Patch by 0znzw & SharkPool
-      SB.SPgradients = { gradientUrls: {}, patched: false };
-      const BSP = SB.BlockSvg.prototype, BSPR = BSP.render;
-      BSP.render = function(...args) {
-        const blockTheme = ReduxStore.getState().scratchGui?.theme?.theme?.blocks;
-        const res = BSPR.apply(this, args);
-        let category;
-        if (this?.svgPath_ && this?.category_ && (category = this.type.slice(0, this.type.indexOf("_"))) && SB.SPgradients.gradientUrls[category]) {
-          const urls = SB.SPgradients.gradientUrls[category];
-          if (urls) {
-            this.svgPath_.setAttribute("fill", urls[0]);
-            if (blockTheme === "dark") {
-              this.svgPath_.setAttribute("fill-opacity", ".5");
-              this.svgPath_.setAttribute("stroke", "#02f5bc");
-            }
-          }
+      /* Gradient Patch by SharkPool, inspired by 0znzw */
+      SB.SPgradients = { gradientUrls: new Map(), patched: true };
+      const ogBlockRender = SB.BlockSvg.prototype.render;
+      SB.BlockSvg.prototype.render = function(...args) {
+        const result = ogBlockRender.apply(this, args);
+        const gradPath = SB.SPgradients.gradientUrls.get(this.type.slice(0, this.type.indexOf("_")));
+        if (gradPath && this?.svgPath_ && this?.category_) {
+          const svg = this.svgPath_;
+          this.svgPath_.setAttribute(
+            svg.getAttribute("fill") === color1 ? "fill" : "stroke",
+            gradPath
+          );
         }
-        return res;
+        return result;
       }
-      SB.SPgradients.patched = true;
     }
-    ScratchBlocks.SPgradients.gradientUrls["SPspotlight"] = ["url(#SPspotlight-GRAD)"];
+    SB.SPgradients.gradientUrls.set("SPcolorMaster", "url(#SPspotlight-GRAD)");
   });
 
   Scratch.extensions.register(new SPspotlight());
