fb636dfcf8e0b62adbe7190b8ea6a3429adeffa6
SharkPool <139097378+SharkPool-SP@users.noreply.github.com>
2026-07-30 16:00:50 -0700

    Image Effects -- Reset context filter when reseting the stage

--- extension-code/Image-Effects.js
+++ extension-code/Image-Effects.js
@@ -4,7 +4,7 @@
 // By: SharkPool
 // Licence: MIT
 
-// Version V.3.0.0
+// Version V.3.0.01
 
 (function (Scratch) {
   "use strict";
@@ -134,6 +134,7 @@
       height += padding;
 
       context.globalCompositeOperation = "source-over";
+      context.filter = "none";
       if (canvas.width === width && canvas.height === height) {
         context.resetTransform();
         context.clearRect(0, 0, width, height);
@@ -205,6 +206,7 @@
       context.scale(dstWidth < 0 ? -1 : 1, dstHeight < 0 ? -1 : 1);
       ImageHelper.drawImage(image, dstWidth, dstHeight);
       context.restore();
+      console.log(context.filter);
     }
 
     static newImage(input) {
